4867c8aff418ee96a2cc71d47289d5e15dfa23db
Nasir <nasirkhimani@gmail.com>
2023-08-09 21:52:11 -0400

    added auto complete

--- src/CodeText.tsx
+++ src/CodeText.tsx
@@ -1,15 +1,36 @@
 import CodeMirror from '@uiw/react-codemirror';
-import {java} from '@codemirror/lang-java'
+import {java} from '@codemirror/lang-java';
 import { duotoneDark } from '@uiw/codemirror-theme-duotone';
+import { autocompletion} from '@codemirror/autocomplete';
+
+
+
 
 const CodeText = (props:any) => {
+
+  function myCompletions(context:any) {
+    let word = context.matchBefore(/\w*/);
+    if (word.from == word.to && !context.explicit) return null;
+  
+    const partialString = word.text; // Get the partial string
+    const computedCompletion = partialString+Date.now(); //TODO: replace the date.now() with the language server result
+  
+    return {
+      from: word.from,
+      options: [
+        { label: computedCompletion, type: "variable" }, // Use computed completion here
+        // Other autocompletion options
+      ],
+    };
+  }
+
   const handleKeyDown = (event:any) => {
     if (event.ctrlKey && event.which === 83) {
       event.preventDefault();
       console.log('save')
       // save here... 
     }
-}
+  }
   return (
     <div className=''>
       <CodeMirror
@@ -18,7 +39,8 @@ const CodeText = (props:any) => {
         basicSetup={{lineNumbers: true}}
         value={props.code}
         theme = {duotoneDark} // TODO: add theme selection functionlity using useState() and mui buttons
-        extensions={[java()]}
+        extensions={[java(), autocompletion({ override: [myCompletions] })
+        ]}
         style={{fontSize: '15px'}}
         onKeyDown={handleKeyDown}
       />
