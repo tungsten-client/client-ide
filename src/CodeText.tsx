7f6983c9ec0f5b818f4e9c874e6b163316fd070c
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2023-09-16 18:49:24 -0400

    add port finding system

--- src/CodeText.tsx
+++ src/CodeText.tsx
@@ -10,50 +10,62 @@ import sampleRust from "../workspace/test-project/src/main/java/org/example/Main
 
 
 const CodeText = (props:any) => {
-  const [ls, setLS] = useState<any>(null)
-  const serverUri = "ws://localhost:9999"
-  //const transport = new WebSocketTransport(serverUri)
-  useEffect(()=>{
-    props.setCode(sampleRust)
-    const newls = languageServer({
-      // WebSocket server uri and other client options.
-      serverUri,
-      rootUri: 'source://test-project',
-      documentUri: 'source://test-project/src/main/java/org/example/Main.java',
-      languageId: 'java', //TODO: Change to java
-      workspaceFolders: null
-    })
-    setLS(newls)
+    const [ls, setLS] = useState<any>(null)
 
-    return () => {
-      
+    let port: number;
+    const loadJavaBridge = () => {
+        if(window.JavaBridge) {
+            port = window.JavaBridge.callJavaFunction("getPort");
+            return;
+        }
+
+        setTimeout(loadJavaBridge, 100);
     }
-  },[])
+    loadJavaBridge();
 
-  
+    const serverUri = `ws://localhost:${port}`
+    //const transport = new WebSocketTransport(serverUri)
+    useEffect(()=>{
+        props.setCode(sampleRust)
+        const newls = languageServer({
+            // WebSocket server uri and other client options.
+            serverUri,
+            rootUri: 'source://test-project',
+            documentUri: 'source://test-project/src/main/java/org/example/Main.java',
+            languageId: 'java', //TODO: Change to java
+            workspaceFolders: null
+        })
+        setLS(newls)
 
-  const handleKeyDown = (event:any) => {
-    if (event.ctrlKey && event.which === 83) {
-      event.preventDefault();
-      console.log('save')
-      // save here... 
+        return () => {
+
+        }
+    },[])
+
+
+
+    const handleKeyDown = (event:any) => {
+        if (event.ctrlKey && event.which === 83) {
+            event.preventDefault();
+            console.log('save')
+            // save here...
+        }
     }
-}
-  return (
-    <div className=''>
-      <CodeMirror
-        height='95vh'
-        width='70vw'
-        basicSetup={{lineNumbers: true}}
-        value={props.code}
-        theme = {duotoneDark} // TODO: add theme selection functionlity using useState() and mui buttons
-        extensions={[java(), ls]}
-        style={{fontSize: '40px'}}
-        onKeyDown={handleKeyDown}
-      />
-    </div>
-  )
-  //potential terminal expansion
+    return (
+        <div className=''>
+            <CodeMirror
+                height='95vh'
+                width='70vw'
+                basicSetup={{lineNumbers: true}}
+                value={props.code}
+                theme = {duotoneDark} // TODO: add theme selection functionlity using useState() and mui buttons
+                extensions={[java(), ls]}
+                style={{fontSize: '40px'}}
+                onKeyDown={handleKeyDown}
+            />
+        </div>
+    )
+    //potential terminal expansion
 }
 
 export default CodeText
